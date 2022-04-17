6155eb03dd6f222ec39206fdda039e320d45fda8
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2022-04-17 10:19:47 +0800

    Add build script, update secret variables format

--- redis-init.sh
+++ redis-init.sh
@@ -17,8 +17,8 @@ function stopRedisAndExit() {
 
 # Turn on job control
 set -m
-# $INIT_PASSWORD will be substitute during build. Refer to scripts/build.sh in this repo
-export REDISCLI_AUTH="$_INIT_PASSWORD"
+# __S_INIT_PASSWORD will be substitute during build. Refer to scripts/build.sh in this repo
+export REDISCLI_AUTH="__S_INIT_PASSWORD"
 
 log "Starting Redis"
 # Put redis-server to background for the meantime
@@ -28,11 +28,11 @@ log "Sleeping for 5 seconds to wait for Redis to fully initialize"
 sleep 5
 
 log "Checking if PSCAN function loaded"
-response="$(redis-cli --user $_INIT_USER FUNCTION LIST LIBRARYNAME pscan)" || stopRedisAndExit
+response="$(redis-cli --user __S_INIT_USER FUNCTION LIST LIBRARYNAME pscan)" || stopRedisAndExit
 
 if [[ "$response" = "" ]]; then
     log "Loading PSCAN function"
-    redis-cli --user init FUNCTION LOAD "$(cat /var/lib/redis/scripts/pscan.lua)" || stopRedisAndExit
+    redis-cli --user __S_INIT_USER FUNCTION LOAD "$(cat /var/lib/redis/scripts/pscan.lua)" || stopRedisAndExit
   else
     log "PSCAN function already loaded"
 fi
---
+++ scripts/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script build this project by replacing secret variables (with __S_ prefix)
+# with their values stored on GitHub
+
+targetFiles=("redis-init.sh" "redis-users.acl")
+
+# $1 is the target file
+# $2 is the string to be replaced
+# $3 is the replacement string
+function replace() {
+  ed -s <<- EOF "$1"
+    ,s/$2/$3/g
+    w
+    q
+	EOF
+}
+
+# Get all secret variables and put them into an array
+secretVariables=()
+while read secret; do
+  secretVariables+=("$secret")
+done < <(grep '__S_*' <(env))
+
+for file in "${targetFiles[@]}"; do
+  for secret in "${secretVariables[@]}"; do
+    echo "Replacing $secret in $file"
+
+    secretName="$(cut -d '=' -f 1 <<< "$secret")"
+    value="$(cut -d '=' -f 2 <<< "$secret")"
+
+    if grep "$secretName" "$file" > /dev/null; then
+      replace "$file" "$secretName" "$value"
+    fi
+  done
+done
+
+echo "Build is completed"
